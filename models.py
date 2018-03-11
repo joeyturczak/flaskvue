549c1fed2415b81a837cab22090b8268743d3395
Joey Turczak <joeyturczak@gmail.com>
2018-03-10 21:46:30 -0800

    remove tables

--- models.py
+++ models.py
@@ -11,24 +11,3 @@ class User(Base):
     name = Column(String(80), nullable=False)
     id = Column(Integer, primary_key=True)
     email = Column(String(80))
-
-class Employee(Base):
-    __tablename__ = 'employee'
-
-    id = Column(Integer, primary_key=True)
-    first_name = Column(String(80), nullable=False)
-    last_name = Column(String(80), nullable=False)
-    employee_id = Column(String(10), nullable=False)
-    start_date = Column(DateTime(timezone=True), default=func.now())
-
-class Attendance(Base):
-    __tablename__ = 'attendance'
-
-    id = Column(Integer, primary_key=True)
-    attendance_type = Column(String(80), nullable=False)
-    date_occurred = Column(DateTime(timezone=True), default=func.now())
-    date_entered = Column(DateTime(timezone=True), default=func.now())
-    user = relationship(User)
-    user_id = Column(Integer, ForeignKey('user.id'))
-    employee = relationship(Employee)
-    employee_id = Column(Integer, ForeignKey('employee.id'))
